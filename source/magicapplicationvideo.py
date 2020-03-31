91ae567839d4137c05b4e5335e77d1b193b0b1fe
kriyanation <homeofmars@gmail.com>
2020-03-31 23:27:31 +0530

    fix for run anywhere -6

--- source/magicapplicationvideo.py
+++ source/magicapplicationvideo.py
@@ -103,7 +103,7 @@ class MagicApplicationVideo(tk.Frame):
         device = config.get("section1", 'device_type')
         if (device == 'rpi'):
             self.notes_text = tk.Text(self.notes_frame,
-                                  font=("TkCaptionFont", 11), foreground="PeachPuff2", width=65, height=8,
+                                  font=("TkCaptionFont", 11), foreground="PeachPuff2", width=60, height=11,
                                   background='dark slate gray',wrap= tk.WORD)
         else:
             self.notes_text = tk.Text(self.notes_frame,
